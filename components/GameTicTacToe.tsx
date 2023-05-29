2c8305e9bbd05bf41ad7dac5bad675ba074d2f00
Francis Targanski <francis@targanski.com>
2023-05-29 16:41:55 -0300

    feature (layout) first definitions

---
+++ components/GameTicTacToe.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/tictactoe.module.css"
+
+const GameTicTacToe = () => {
+  return (
+    <>
+      <main className={styles.main}>Hello</main>
+    </>
+  );
+};
+
+export default GameTicTacToe;
---
+++ components/HeadSite.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+
+const HeadSite = () => {
+  return (
+    <>
+      <Head>
+        <title>Tic Tac Toe</title>
+        <meta name="description" content="Tic Tac Toe" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="../images/favicon.svg" />
+        <meta name="keywords" content="Tic Tac Toe" />
+      </Head>
+    </>
+  );
+};
+
+export default HeadSite;
